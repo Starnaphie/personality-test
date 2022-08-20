1e6515c17e5efa2c0a682dee353f777636190e6d
Starnaphie <41340960+Starnaphie@users.noreply.github.com>
2022-08-19 17:04:23 -0700

    Revert "changes for v2"
    This reverts commit 937378c059d115b5837a3f8d2347c2d8a3cedd30.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,15 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import ReactDOM from 'react-dom';
-import React from 'react';
-=======
-=======
->>>>>>> parent of 937378c0 (changes for v2)
-=======
->>>>>>> parent of 937378c0 (changes for v2)
 import logo from './logo.svg';
->>>>>>> parent of 937378c0 (changes for v2)
 import './App.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
-
-function App() { 
-  <div>
-    <h1>Hello, world!</h1>
-    <h2>It is {new Date().toLocaleTimeString()}.</h2>
-  </div>
-}
-
-setInterval(App, 1000);
-
-function Question() {
+function App() {
   return (
-<<<<<<< HEAD
-    <></>
-  );
-}
-
- export default App;
-=======
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello from V2</h1>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 937378c0 (changes for v2)
-=======
->>>>>>> parent of 937378c0 (changes for v2)
-=======
->>>>>>> parent of 937378c0 (changes for v2)
