eeca2f4aca430722823bf7cd818f3c62456ab4b2
Starnaphie <41340960+Starnaphie@users.noreply.github.com>
2022-08-24 15:14:15 -0700

    header??

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Question from './Question.js'
 
 export default function QuestionList() {
   return (
-    <div>QuestionList</div>
+    <div>
+        
+    </div>
   )
 }
